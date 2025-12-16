a2c80bef4634d3bf468cfe2c4ff404ad8a7e2f4b
Annabel <annabelpeart@hotmail.com>
2025-12-16 09:25:08 +0000

    (WIP) feat: added basic routes to server.js - requires additional data

--- server/server.js
+++ server/server.js
@@ -19,3 +19,28 @@ app.listen(PORT, function () {
 app.get("/", (req, res) => {
   res.json({ message: "Root route" });
 });
+
+// !THE BELOW REQUIRES SOME DATA TO BE ADDED TO MATCH WITH THE DATABASE
+
+//TODO: Construct GET route to obtain ALL database data
+app.get("/stock", async function (_, res) {
+  const query = await db.query(
+    `SELECT col1, col2, col3, col4, col5 FROM ${databaseName};`
+  );
+  console.log(query);
+  res.json(query.rows);
+});
+
+//TODO: Construct POST route to post a new item to the database
+app.post("/add", (req, _) => {
+  const addStock = req.body.formValues;
+  console.log(addStock);
+
+  const query = db.query(
+    `INSERT INTO ${databaseName} (col1, col2, col3, col4, col5) VALUES ($1, $2, $3, $4)`,
+    [addStock.col1, addStock.col2, addStock.col3, addStock.col4, addStock.col5]
+  );
+  console.log(query);
+
+  _.json({ status: "success", value: addStock });
+});
