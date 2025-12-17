2d8cd6b941d7c0f917f3aa63803ee83611bbd12b
Annabel <101633619+Angel-Hime@users.noreply.github.com>
2025-12-17 09:25:40 +0000

    app.js fix - Implement modal form for adding new items
    Added functionality to create and display a modal form for adding items, including input fields for item details and event listeners for form submission and modal closure.

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,256 @@
 console.log("Hello World!");
 
+//!temp button
+const addItemButton = document.createElement("button");
+addItemButton.textContent = "add item";
+document.body.appendChild(addItemButton);
+
+//const addItemButton = document.getElementById("add");
+
+// TODO: Create the modal and the form once the button has been clicked
+addItemButton.addEventListener("click", () => {
+  const submitSound = document.createElement("audio");
+  submitSound.src = "client/public/audio/add-popup-swoosh.wav";
+  submitSound.volume = 0.5;
+  submitSound.play();
+
+  //construct modal
+  const addItemModal = document.createElement("div");
+  addItemModal.id = "addItemModal";
+  addItemModal.className = "addItemModal";
+  addItemModal.style.display = "block";
+  //append modal to body
+  document.body.appendChild(addItemModal);
+  //construct section for form to nest in
+  const outerSection = document.createElement("section");
+  outerSection.id = "outerSection";
+  outerSection.className = "outerSection";
+  //append form section to modal
+  addItemModal.appendChild(outerSection);
+  //construct close feature
+  const closeModal = document.createElement("span");
+  closeModal.id = "close";
+  closeModal.className = "close";
+  closeModal.textContent = "❌";
+  //apend close to form section
+  outerSection.appendChild(closeModal);
+
+  //call function to create form and parse through the section of the modal in which we want to append the form
+  createForm(outerSection);
+
+  //close modal with span
+  closeModal.addEventListener("click", () => {
+    addItemModal.style.display = "none";
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target === addItemModal) {
+      addItemModal.style.display = "none";
+    }
+  });
+});
+
+function createForm(outerSection) {
+  //construct section for form to nest in
+  const formSection = document.createElement("section");
+  formSection.id = "formSection";
+  formSection.className = "formSection";
+
+  //construct the form to add item
+  const addItemForm = document.createElement("form");
+
+  //add in the inputs and labels for form
+  const itemNameLabel = document.createElement("label");
+  itemNameLabel.htmlFor = "item-name";
+  itemNameLabel.textContent = "Item Name: ";
+  const itemNameInput = document.createElement("input");
+  itemNameInput.name = "item-name";
+  itemNameInput.type = "text";
+  itemNameInput.placeholder = "e.g. bread";
+  itemNameInput.required = true;
+  addItemForm.appendChild(itemNameLabel);
+  addItemForm.appendChild(itemNameInput);
+
+  const quantityLabel = document.createElement("label");
+  quantityLabel.htmlFor = "quantity";
+  quantityLabel.textContent = "Quantity: ";
+  const quantityInput = document.createElement("input");
+  quantityInput.name = "quantity";
+  quantityInput.placeholder = "0";
+  quantityInput.type = "number";
+  quantityInput.min = "1";
+  quantityInput.required = true;
+  addItemForm.appendChild(quantityLabel);
+  addItemForm.appendChild(quantityInput);
+
+  const unitTypeLabel = document.createElement("label");
+  unitTypeLabel.htmlFor = "unit-type";
+  unitTypeLabel.textContent = "Unit Type: ";
+  const unitTypeInput = document.createElement("select");
+  unitTypeInput.name = "unit-type";
+  unitTypeInput.required = true;
+  unitTypeInput.placeholder = "unit";
+  const optG = document.createElement("option");
+  optG.value = "grams";
+  optG.text = "grams";
+  const optKG = document.createElement("option");
+  optKG.value = "kilograms";
+  optKG.text = "kilograms";
+  const optML = document.createElement("option");
+  optML.value = "mililetres";
+  optML.text = "mililetres";
+  const optL = document.createElement("option");
+  optL.value = "litres";
+  optL.text = "litres";
+  const optItems = document.createElement("option");
+  optItems.value = "items";
+  optItems.text = "items";
+  unitTypeInput.appendChild(optG);
+  unitTypeInput.appendChild(disposables);
+  unitTypeInput.appendChild(optML);
+  unitTypeInput.appendChild(optL);
+  unitTypeInput.appendChild(optItems);
+  addItemForm.appendChild(unitTypeLabel);
+  addItemForm.appendChild(unitTypeInput);
+
+  const categoryLabel = document.createElement("label");
+  categoryLabel.htmlFor = "category";
+  categoryLabel.textContent = "Category: ";
+  const categoryInput = document.createElement("select");
+  categoryInput.name = "category";
+  categoryInput.required = true;
+  const optC1 = document.createElement("option");
+  optC1.value = " ";
+  optC1.text = " ";
+  const optC2 = document.createElement("option");
+  optC2.value = " ";
+  optC2.text = " ";
+  const optC3 = document.createElement("option");
+  optC3.value = " ";
+  optC3.text = " ";
+  const optC4 = document.createElement("option");
+  optC4.value = " ";
+  optC4.text = " ";
+  const optC5 = document.createElement("option");
+  optC5.value = " ";
+  optC5.text = " ";
+  unitTypeInput.appendChild(optC1);
+  unitTypeInput.appendChild(optC2);
+  unitTypeInput.appendChild(optC3);
+  unitTypeInput.appendChild(optC4);
+  unitTypeInput.appendChild(optC5);
+  addItemForm.appendChild(unitTypeLabel);
+  addItemForm.appendChild(unitTypeInput);
+
+  const subcategoryLabel = document.createElement("label");
+  const subcategoryInput = document.createElement("input");
+  const optSC = document.createElement("option");
+  optSC.value = " ";
+  optSC.text = " ";
+  const optSC2 = document.createElement("option");
+  optSC2.value = " ";
+  optSC2.text = " ";
+  const optSC3 = document.createElement("option");
+  optSC3.value = " ";
+  optSC3.text = " ";
+  const optSC4 = document.createElement("option");
+  optSC4.value = " ";
+  optSC4.text = " ";
+  const optSC5 = document.createElement("option");
+  optSC5.value = " ";
+  optSC5.text = " ";
+  unitTypeInput.appendChild(optSC);
+  unitTypeInput.appendChild(optSC2);
+  unitTypeInput.appendChild(optSC3);
+  unitTypeInput.appendChild(optSC4);
+  unitTypeInput.appendChild(optSC5);
+  addItemForm.appendChild(unitTypeLabel);
+  addItemForm.appendChild(unitTypeInput);
+
+  addItemForm.appendChild(subcategoryLabel);
+  addItemForm.appendChild(subcategoryInput);
+
+  const storageLocationLabel = document.createElement("label");
+  const storageLocationInput = document.createElement("input");
+  const optPantry = document.createElement("option");
+  optPantry.value = "pantry";
+  optPantry.text = "Pantry";
+  const optDisposables = document.createElement("option");
+  optDisposables.value = "disposables";
+  optDisposables.text = "Disposables";
+  const optFridge = document.createElement("option");
+  optFridge.value = "Fridge";
+  optFridge.text = "Fridge";
+  const optFreezer = document.createElement("option");
+  optFreezer.value = "freezer";
+  optFreezer.text = "Freezer";
+  const optOther = document.createElement("option");
+  optOther.value = "optOther";
+  optOther.text = "Other";
+  storageLocationInput.appendChild(optPantry);
+  storageLocationInput.appendChild(optDisposables);
+  storageLocationInput.appendChild(optFridge);
+  storageLocationInput.appendChild(optFreezer);
+  storageLocationInput.appendChild(optOther);
+  addItemForm.appendChild(storageLocationLabel);
+  addItemForm.appendChild(storageLocationInput);
+
+  const expiryDateLabel = document.createElement("label");
+  expiryDateLabel.htmlFor = "date";
+  expiryDateLabel.textContent = "Enter a date (YYYY-MM-DD)";
+  const expiryDateInput = document.createElement("input");
+  expiryDateInput.type = "date";
+  expiryDateInput.name = "date";
+  expiryDateInput.pattern = "d{4}-d{2}-d{2}";
+  expiryDateInput.required = true;
+  addItemForm.appendChild(storageLocationLabel);
+  addItemForm.appendChild(storageLocationInput);
+
+  const commentsLabel = document.createElement("label");
+  commentsLabel.htmlFor = "text";
+  commentsLabel.textContent = "Additional Comments: ";
+  const commentsInput = document.createElement("textarea");
+  commentsInput.maxLength = 1000;
+
+  addItemForm.appendChild(commentsLabel);
+  addItemForm.appendChild(commentsInput);
+
+  //append form to form section
+  formSection.appendChild(addItemForm);
+
+  outerSection.appendChild(formSection);
+
+  addItemForm.addEventListener("submit", handleAddNewItemFormSubmit);
+}
+
+async function handleAddNewItemFormSubmit(event) {
+  event.preventDefault();
+  const formDataTemplate = new FormData(addItemForm);
+  const formValues = Object.fromEntries(formDataTemplate);
+
+  //sound on click
+  //   const submitSound = document.createElement("audio");
+  //   submitSound.src = "";
+  //   submitSound.volume = 0.5;
+  //   submitSound.play();
+
+  // this is to see our posted data
+  console.log(
+    `${formValues.item_name} ${formValues.quantity} ${formValues.unit_type} ${formValues.category} ${formValues.subcategory}${formValues.storage_location}${formValues.expiry_Date} ${formValues.comments}`
+  );
+
+  // fetch the POST server route
+  fetch("http://localhost:8080/add_item", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ formValues }),
+  });
+
+  //TODO: clear form and close modal on submit - no need to make a new one
+}
+
 //functions for all possible button interactions goes here:
 function donate() {
   alert("To donation page");
