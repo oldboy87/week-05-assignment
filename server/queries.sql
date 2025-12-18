65121b73f9206a8bdbc453b6a7974404ef8b019d
Annabel <annabelpeart@hotmail.com>
2025-12-18 13:52:27 +0000

    WIP feat: get data refresh for updated database

--- server/queries.sql
+++ server/queries.sql
@@ -5,8 +5,9 @@
 CREATE TABLE inventory (  
   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
   item_name VARCHAR(255),
-  date_added DATE,
+  date_added DATE DEFAULT current_date,
   quantity INT,
+  unit_size INT,
   unit_type VARCHAR(255),
   category VARCHAR(255),
   subcategory VARCHAR(255), 
@@ -16,9 +17,9 @@ CREATE TABLE inventory (
 );
 
 -- TODO: Add an entry
-INSERT INTO inventory (item_name, date_added, quantity, unit_type, category, subcategory, storage_location, expiry_date, comments) VALUES 
-('testItem', '2025-12-16', 10, 'L', 'beverages', 'can drinks', 'pantry', '2027-12-01', 'testComment')
+INSERT INTO inventory (item_name, quantity, unit_size, unit_type, category, subcategory, storage_location, expiry_date, comments) VALUES 
+('testItem', 10, 1, 'L', 'beverages', 'can drinks', 'pantry', '2027-12-01', 'testComment')
 
 -- above translated into generic code for JS use
-`INSERT INTO inventory (item_name, date_added, quantity, unit_type, category, subcategory, storage_location, expiry_date, comments) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
+`INSERT INTO inventory (item_name, quantity, unit_size, unit_type, category, subcategory, storage_location, expiry_date, comments) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
 [addStock.col1, addStock.col2, addStock.col3, addStock.col4, addStock.col5, addStock.col6, addStock.col7, addStock.col8, addStock.col9] -- generic for your ease of use for now 
